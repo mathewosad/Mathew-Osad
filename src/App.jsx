96533331e1bb3004d90cd77ae7228604e4dab12f
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-04-10 22:21:49 -0700

    conecting my tabs and testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
 
 
 
@@ -9,6 +17,14 @@ const App = () => {
       <>
         <Header />
         <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+
       </>  
 )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import ReactDom from 'react-dom';
-import App from './App.jsx';
+import ReactDom from 'react-dom'
+import App from './App'
 
-import './index.css';
+import './index.css'
 
-ReactDom.render(<app/>, document.querySelector('#root'));
+ReactDom.render(<App/>, document.querySelector('#root'));
