89b4fa2de2e6e6754d6193eb83c77c9e3a855d6b
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-04-10 22:06:38 -0700

    creating more components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-// import './App.css'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+
+
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+      <>
+        <Header />
+        <Nav />
+      </>  
+)
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDom from 'react-dom';
-import App from './App';
+import App from './App.jsx';
+
 import './index.css';
 
 ReactDom.render(<app/>, document.querySelector('#root'));
