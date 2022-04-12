0ac1cd24999931dbb7ce01843743d68523dfaa0f
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-04-12 07:12:55 -0700

    adding header components header socials and making changes on my index.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,10 +12,13 @@ const Header = () => {
         <h1>Mathew Osad</h1>
         <h5 className="text-light">FullStack Developer</h5>
         <CTA />
+        <HeaderSocials />
 
         <div className="me">
           <img src={ME} alt="me" />
         </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
     </header>
   )
 }
