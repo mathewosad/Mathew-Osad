86088126db9d344423839dba508dfacdc41e90e2
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-04-12 09:29:31 -0700

    Nav bar icons in progress

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {RiUserReceivedLine} from 'react-icons/ri'
+import {ImStack} from 'react-icons/im'
+import {FaRegHandshake} from 'react-icons/fa'
+import {TiMessages} from 'react-icons/ti'
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><RiUserReceivedLine/></a>
+      <a href="#experience"><ImStack/></a>
+      <a href="#services"><FaRegHandshake/></a>
+      <a href="#contact"><TiMessages/></a>
+    </nav>
   )
 }
 
