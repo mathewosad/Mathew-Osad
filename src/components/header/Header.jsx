790f065a39605d435093f03c798c806636b86c58
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-04-11 19:08:24 -0700

    added images

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
@@ -10,6 +11,10 @@ const Header = () => {
         <h1>Mathew Osad</h1>
         <h5 className="text-light">FullStack Developer</h5>
         <CTA />
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
     </header>
   )
 }
