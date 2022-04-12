2e6a53661d1beeda7015276c34845622611b5f3d
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-04-11 18:24:43 -0700

    added CTA file for my resume download

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Resume from '../../assets/resume.pdf'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href={Resume} download>Download Resume</a>
+        <a href="#contact">Let's Chat</a>
+    </div>
+  )
+}
+
+export default CTA
