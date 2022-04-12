2b91a9871213a74ec1ce10e3a84450affcdad80b
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-04-12 10:01:48 -0700

    working on my NavBar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,13 +5,16 @@ import {RiUserReceivedLine} from 'react-icons/ri'
 import {ImStack} from 'react-icons/im'
 import {FaRegHandshake} from 'react-icons/fa'
 import {TiMessages} from 'react-icons/ti'
+import { useState } from 'react'
 
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><RiUserReceivedLine/></a>
+      <a href="#" className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><RiUserReceivedLine/></a>
       <a href="#experience"><ImStack/></a>
       <a href="#services"><FaRegHandshake/></a>
       <a href="#contact"><TiMessages/></a>
