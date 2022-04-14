cc3fc74777c8c2d6980d8656c4ac0c46118b5e14
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-04-13 18:06:42 -0700

    finished floating NavBar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -12,12 +12,12 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
 
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><RiUserReceivedLine/></a>
-      <a href="#experience"><ImStack/></a>
-      <a href="#services"><FaRegHandshake/></a>
-      <a href="#contact"><TiMessages/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><ImStack/></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><FaRegHandshake/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TiMessages/></a>
     </nav>
   )
 }
