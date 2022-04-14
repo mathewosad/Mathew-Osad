f7370afd4228a244323a8dd23a4a891d6e791605
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-04-13 22:33:24 -0700

    finished up the experience.css and fixed couple of errors on my index.css and expeience.jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import './experience.css'
+import {AiOutlineHtml5} from 'react-icons/ai'
+import {FaCss3Alt} from 'react-icons/fa'
+import {BsBootstrap} from 'react-icons/bs'
+import {SiNodedotjs} from 'react-icons/si'
+import {SiMysql} from 'react-icons/si'
+import {SiExpress} from 'react-icons/si'
 import {GiCheckeredFlag} from 'react-icons/gi'
+import {DiJavascript1} from 'react-icons/di'
+import {SiTailwindcss} from 'react-icons/si'
+import {FaReact} from 'react-icons/fa'
+import {GrGraphQl} from 'react-icons/gr'
+import {SiMongodb} from 'react-icons/si'
+
 
 const Experience = () => {
   return (
@@ -13,42 +25,42 @@ const Experience = () => {
           <h3>Front-End Development</h3>
           <div className="experience__content">
             <article className='experience__details'>
-              <GiCheckeredFlag className='experience__details-icon'/>
+              <AiOutlineHtml5 className='experience__details-icon'/>
               <div>
                 <h4>HTML</h4>
                 <small className='text-light'>Expert</small>
               </div>
             </article>
             <article className='experience__details'>
-              <GiCheckeredFlag className='experience__details-icon'/>
+              <FaCss3Alt className='experience__details-icon'/>
               <div>
                 <h4>CSS</h4>
                 <small className='text-light'>Intermidiate</small>
               </div>
             </article>
             <article className='experience__details'>
-              <GiCheckeredFlag className='experience__details-icon'/>
+              <DiJavascript1 className='experience__details-icon'/>
               <div>
                 <h4>JavaScript</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <GiCheckeredFlag className='experience__details-icon'/>
+              <BsBootstrap className='experience__details-icon'/>
               <div>
                 <h4>BootStarp</h4>
                 <small className='text-light'>Expert</small>
               </div>
             </article>
             <article className='experience__details'>
-              <GiCheckeredFlag className='experience__details-icon'/>
+              <SiTailwindcss className='experience__details-icon'/>
               <div>
                 <h4>TailWind</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <GiCheckeredFlag className='experience__details-icon'/>
+              <FaReact className='experience__details-icon'/>
               <div>
                 <h4>REACT</h4>
                 <small className='text-light'>Experienced</small>
@@ -62,28 +74,28 @@ const Experience = () => {
         <h3>Back-End Development</h3>
           <div className="experience__content">
             <article className='experience__details'>
-              <GiCheckeredFlag className='experience__details-icon'/>
+              <SiNodedotjs className='experience__details-icon'/>
               <div>
                 <h4>NodeJS</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <GiCheckeredFlag className='experience__details-icon'/>
+              <SiMysql className='experience__details-icon'/>
               <div>
                 <h4>MYSQL</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <GiCheckeredFlag className='experience__details-icon'/>
+              <SiExpress className='experience__details-icon'/>
               <div>
                 <h4>Express</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <GiCheckeredFlag className='experience__details-icon'/>
+              <GrGraphQl className='experience__details-icon'/>
               <div>
                 <h4>GraphQL</h4>
                 <small className='text-light'>Experienced</small>
@@ -95,7 +107,7 @@ const Experience = () => {
               <small className='text-light'>Experienced</small>
             </article> */}
             <article className='experience__details'>
-              <GiCheckeredFlag className='experience__details-icon'/>
+              <SiMongodb className='experience__details-icon'/>
               <div>
                 <h4>MongoDB</h4>
                 <small className='text-light'>Experienced</small>
