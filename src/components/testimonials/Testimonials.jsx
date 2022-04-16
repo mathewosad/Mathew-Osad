083a9f2ba06420b236b612dd6e092d22c3bf8309
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-04-15 20:00:29 -0700

    finished up testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import './testimonials.css'
+import AVATAR1 from '../../assets/avatar1.jpg'
+import AVATAR2 from '../../assets/avatar2.jpg'
+import AVATAR3 from '../../assets/avatar3.jpg'
+import AVATAR4 from '../../assets/avatar4.jpg'
+
+const data = [
+  { 
+    avatar: AVATAR1,
+    name: 'VIsion Dee',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    avatar: AVATAR2,
+    name: 'Hajia Bintu',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    avatar: AVATAR3,
+    name: 'Ryan Achu',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  { 
+    avatar: AVATAR4,
+    name: 'Ernesto Sanchez',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials </section>
+    <section id='testimonials'>
+      <h5>Clients Reviews</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        {
+          data.map(({ avatar, name, review }, index) => {
+            return (
+              <article key={index} className='testimonial'>
+              <div className='client__avatar'>
+                <img src={avatar} />
+              </div>
+              <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+            </article>
+            )
+          })
+        }
+
+      </div>
+    </section>
   )
 }
 
