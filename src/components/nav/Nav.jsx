afbd02d150e7136698d9c254e8d1a8f6fc10f504
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-04-17 15:00:10 -0700

    fixing and rearranging bugs and polishing

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,6 +4,7 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {RiUserReceivedLine} from 'react-icons/ri'
 import {ImStack} from 'react-icons/im'
 // import {FaRegHandshake} from 'react-icons/fa'
+import {AiOutlineLaptop} from 'react-icons/ai'
 import {TiMessages} from 'react-icons/ti'
 import { useState } from 'react'
 
@@ -17,6 +18,7 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><RiUserReceivedLine/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><ImStack/></a>
       {/* <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><FaRegHandshake/></a> */}
+      <a href="#portfolio" onClick={() => setActiveNav('#Portfolio')} className={activeNav === '#Portfolio' ? 'active' : ''}><AiOutlineLaptop/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TiMessages/></a>
     </nav>
   )
